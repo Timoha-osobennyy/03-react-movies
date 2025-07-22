88e5572c546162fc73557848cca5de48a6a96947
Tymofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-07-22 15:46:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,22 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const base = env.VERCEL ? '/' : env.GH_PAGES ? '/03-react-movies/' : '/'
+  const base = env.VERCEL ? '/' : '/03-react-movies/'
 
   return {
     base,
-    root: fileURLToPath(new URL('./', import.meta.url)),
-    publicDir: fileURLToPath(new URL('./public', import.meta.url)),
     plugins: [react()],
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    },
     server: {
-      open: '/index.html',
-      port: 3000,
-      strictPort: true,
-      host: true
+      open: true,
+      port: 3000
     },
     build: {
-      outDir: fileURLToPath(new URL('./dist', import.meta.url)),
+      outDir: 'dist',
       emptyOutDir: true,
+      chunkSizeWarningLimit: 2000,
       rollupOptions: {
-        input: fileURLToPath(new URL('./public/index.html', import.meta.url)),
         output: {
           assetFileNames: 'assets/[name]-[hash][extname]',
           chunkFileNames: 'assets/[name]-[hash].js',
